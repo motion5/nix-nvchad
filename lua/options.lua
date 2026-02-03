2573f8b74b2645a83368ba8574319aee80f99674
Rob.Fallaize <fallaizerob@gmail.com>
2026-02-03 22:16:04 +0000

    feat(autoread): Add autoread on buffer focus
    Signed-off-by: Rob.Fallaize <fallaizerob@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -31,3 +31,10 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.bicep",
   command = "set filetype=bicep",
 })
+
+-- Enable autoread and set up checking triggers
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = "*",
+})
